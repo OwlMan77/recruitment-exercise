5d28d2e28ee300ca6cd5cf0e4cb3deeef0bbc875
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-05-09 16:47:26 +0100

    got the donut grap to show with JSON data

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -5,91 +5,109 @@ import 'rxjs/add/operator/map';
 import * as c3 from 'c3';
 
 @IonicPage()
+
 @Component({
   selector: 'page-dashboard',
   templateUrl: 'dashboard.html',
 })
 
+
+
 export class Dashboard {
+line: any;
+donut: any
+graphDataDonut: any;
+graphDataNames: any;
+graphData: any;
   @ViewChild('dashboardDonut') dashboardDonut: ElementRef;
   //added child selector line graph
   @ViewChild('dashboardLine') dashboardLine: ElementRef;
   //Private property on http:Http, to make http available to the whole component
   constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http) {
-  this.http;
+    this.http;
+    this.line;
+    this.donut;
+    this.graphDataDonut;
+    this.graphDataNames;
+    this.graphData;
   }
 
-  //Data store
-  private today = new Date().toJSON().split('T')[0];
-  private graphData = [];
-  private graphDataNames = [];
-  private graphDataDonut = {};
 
   //API call function
   getInfo(){
+
+    //gets the values from the two date fields then maps them to be
     let Date1 = (<HTMLInputElement>document.getElementById("startDate")).value;
     let Date2 = (<HTMLInputElement>document.getElementById("endDate")).value;
-    //gets the values from the two date fields then maps them to be
-
+    this.graphDataDonut = {};
+    this.graphDataNames = [];
+    this.graphData      = {Unavailable:0};
     this.http.get(`https://test-calendar.herokuapp.com/?from=${Date1}&to=${Date2}`).map(res => res.json())
       .subscribe(data => {
-      // display raw data to show it is working
-
+        //display raw data to show it is working
       //loops through the data based off the number of it's subkeys and pushes it into an array
-      for (let i = 0; i < Object("data").length; i++){
-        this.graphData.push(data.calendar[i]);
-
-        if (this.graphDataDonut[data.calendar[i].source] != null ){
-          this.graphDataDonut[data.calendar[i].source].push(data.calendar[i].gbp_price);
-        }
-        else if( data.calendar[i].source != null && data.calendar[i].gbp != null){
-          this.graphDataDonut[data.calendar[i].source] = [];
-          this.graphDataDonut[data.calendar[i].source].push(data.calendar[i].gbp_price);
-        }
-
-        if(this.graphDataNames.indexOf(data.calendar[i].source) === -1){
-          this.graphDataNames.push(data.calendar[i].source);
-        }
+      for (let i = 0; i < data.calendar.length; i++){
+        this.dataAssign(data.calendar[i]);
+        this.dataNameAssign(data.calendar[i]);
       }
-      console.log(this.graphData);
-      console.log(this.graphDataDonut);
+          })
+
+
+      //console.log(this.graphData);
+      //console.log(this.graphDataDonut);
+      //console.log(this.graphDataNames);
     });
   };
 
-ionViewDidLoad() {
-  let dashboardLineArea = this.dashboardLine.nativeElement;
-  let dashboardDonutArea = this.dashboardDonut.nativeElement;
+  dataAssign(data){
+      if (this.graphData[data.source] == null && data.source != null){
+        this.graphData[data.source] = data.nights;
+      }
+      else if( data.source != null && data.nights != null){
+        this.graphData[data.source] += data.nights;
+      }
+      else if( data.source == null && data.nights != null ){
+      console.log(this.graphData['Unavailable']);
+        this.graphData['Unavailable'] += data.nights;
+      }
+  }
 
+  dataNameAssign(data){
+    if (this.graphDataNames.indexOf(data.source) === -1 && data.source != null){
+    this.graphDataNames.push(data.source);
+    }
+    else if (this.graphDataNames.indexOf('Unavailable') === -1){
+      this.graphDataNames.push('Unavailable');
+    }
+  }
+
+
+    //buildLineGraph(){
+      //let dashboardLineArea = this.dashboardLine.nativeElement;
+      //this.line = c3.generate({
+      //bindto: dashboardLineArea,
+      //data: {
+      //json: this.graphData,
+      //keys: {
+      //          x: 'date',
+      //          value:['gbp_price']
+      //          }
+      //        },
+      //         axis: {
+      //          x: {
+      //           type: 'line'
+      //          }
+      //        }
+      //      });
+    //}
+
+
+ionViewWillEnter() {
+  //console.log(this.graphData);
   //Line Chart
-   var line = c3.generate({
-    bindto: dashboardLineArea,
-    data: {
-      json: this.graphData,
-      keys: {
-        x: 'date',
-        value:['gbp_price']
-      }
-    },
-     axis: {
-      x: {
-       type: 'line'
-      }
-   }
-  });
-
+  //this.buildLineGraph();
   //Donut Chart
-    var donut = c3.generate({
-        bindto: dashboardDonutArea,
-        data: {
-            type: 'donut',
-            json: this.graphData,
-            keys: {
-              value: this.graphDataNames,
-            }
-        },
-        donut: {
-            title: (100).toString()
-        }
-    });
+  e§
 }
+
 }
